7e82cb983b34f2be5094a12fd8571c26527461bb
irikuz <i-iri.kuz@yandex.ru>
2024-12-02 17:28:35 +0300

    module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get("/")
-async def Get_Main_Page() -> dict:
+async def get_main_page() -> dict:
     return {"message": "Главная страница"}
 
 
 @app.get("/user/admin")
-async def Get_Admin_Page() -> dict:
+async def get_admin_page() -> dict:
     return {"message": "Вы вошли как администратор"}
 
 
 @app.get("/user/{user_id}")
-async def Get_User_Number(user_id: str) -> dict:
+async def get_user_number(user_id: str) -> dict:
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 
-@app.get("/user/{user_name}/{age}")
-async def Get_User_Info (user_name: str, age: str) -> dict:
+@app.get("/user}")
+async def get_user_info (user_name: str, age: int) -> dict:
     return {"message": f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"}

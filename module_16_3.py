73e9a81dc0584a89242ab792327d2b999b1b36a5
irikuz <i-iri.kuz@yandex.ru>
2024-12-05 17:04:54 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -19,7 +19,7 @@ async def user_register(username: Annotated[str, Path(min_length=3, max_length=1
     return f'User {user_id} is registered'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str = Path(ge=1, le=100, description='Enter your age', example='24')
+async def update_user(user_id: int = Path(ge=1, le=100, description='Enter your id', example='20')
                       , username: str = Path(min_length=3, max_length=20, description='Enter username',
                                              example='UrbanUser')
                       , age: int = 30) -> str:
